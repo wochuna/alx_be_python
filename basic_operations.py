3601e7cbf5fa5bac8994f86ed1ef8dd2839db2d4
wochuna <wochunayvonne@gmail.com>
2025-09-09 21:40:26 +0300

    Operations in basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,17 @@
+# Variable assignment
+number1 = 10
+number2 = 5
+
+# Addition
+Addition_result = number1 + number2
+
+# Subtraction
+Subtraction_result = number1 - number2
+
+# Multiplication
+Multiplication_result = number1 * number2
+
+# Operation Results
+print(f"Addition of {number1} and {number2} is {Addition_result}")
+print(f"Subtraction of {number1} and {number2} is {Subtraction_result}")
+print(f"Multiplication of {number1} and {number2} is {Multiplication_result}")
