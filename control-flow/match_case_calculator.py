3cbb147b547bd5ad5e799db6803b6646c6d3a72b
wochuna <wochunayvonne@gmail.com>
2025-09-20 22:01:31 +0300

    Removed function definition

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -1,27 +1,21 @@
-def main():
-    # Prompt for User Input
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-    operator = input("Choose the operation (+, -, *, /): ")
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
 
-    match operator:
-        case '+':
-            result = num1 + num2
+match operator:
+    case '+':
+        result = num1 + num2
+        print(f"The result is {result}.")
+    case '-':
+        result = num1 - num2
+        print(f"The result is {result}.")
+    case '*':
+        result = num1 * num2
+        print(f"The result is {result}.")
+    case '/':
+        if num2 == 0:
+            print("Cannot divide by zero.")
+        else:
+            result = num1 / num2
             print(f"The result is {result}.")
-        case '-':
-            result = num1 - num2
-            print(f"The result is {result}.")
-        case '*':
-            result = num1 * num2
-            print(f"The result is {result}.")
-        case '/':
-            if num2 == 0:
-                print("Cannot divide by zero.")
-            else:
-                result = num1 / num2
-                print(f"The result is {result}.")
-        case _:
-            print("Invalid operation. Please choose +, -, *, or /.")
-
-if __name__ == "__main__":
-    main()
+            
