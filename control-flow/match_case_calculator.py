0e0ebe5f85aeb605dd68012dfa13e1dec2bcf05a
wochuna <wochunayvonne@gmail.com>
2025-09-20 22:03:52 +0300

    Add a case for invalid input

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -18,4 +18,6 @@ match operator:
         else:
             result = num1 / num2
             print(f"The result is {result}.")
-            
+    case _:
+        print("Invalid operation. Please choose +, -, *, or /.")
+    
