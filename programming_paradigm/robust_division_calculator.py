2e5376100152e2d7d27633db0e6caec5bca4c451
wochuna <wochunayvonne@gmail.com>
2025-10-04 14:37:18 +0300

    Add calculator handling ZeroDivisionError and ValueError

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,11 @@
+def safe_divide(numerator, denominator):
+    try:
+        numerator_float = float(numerator)
+        denominator_float = float(denominator)
+        result = numerator_float/denominator_float
+    except ZeroDivisionError:
+        return "Error: Cannot divide by zero"
+    except ValueError:
+        return "Error: Please enter numeric values only"
+    else:
+        print(f"The result of the division is {result}")
